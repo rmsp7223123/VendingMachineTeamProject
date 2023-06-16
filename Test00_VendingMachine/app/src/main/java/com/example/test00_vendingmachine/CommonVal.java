fc0aaed7d2121e07fc9e854bd230ba0033ac82f1
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-06-16 15:06:35 +0900

    ++

--- Test00_VendingMachine/app/src/main/java/com/example/test00_vendingmachine/CommonVal.java
+++ Test00_VendingMachine/app/src/main/java/com/example/test00_vendingmachine/CommonVal.java
@@ -1,9 +1,9 @@
 package com.example.test00_vendingmachine;
 
 public class CommonVal {
-    public static String[] names = { "콜라" , "사이다" , "탐스" , "솔의눈"};
-    public static int[] cnt = { 1 , 2 , 3 , 4};
-    public static int[] price = {1000 , 800 , 500 ,200 };
+    public static String[] names = { "설탕커피","블랙커피","밀크커피","율무차"};
+    public static int[] cnt = { 12 , 8 , 4 , 9};
+    public static int[] price = {500 , 500 , 600 ,700 };
 
 
 }
